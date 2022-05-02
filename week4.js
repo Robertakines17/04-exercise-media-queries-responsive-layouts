3c08c1b3388186446ab0f088fbf2c54c7c98fa48
Robert Akines Jr <robert.akines17@yahoo.com>
2022-05-01 20:06:24 -0500

    04-exercise-js-good-grades

---
+++ week4.js
@@ -0,0 +1,8 @@
+var num1 = 50;
+let num2 = Number(prompt ("Enter Number Of Points Missed: ")); 
+var num3 = num1-num2;
+var num4= num3/num1;
+var num5= num4 * 100;
+console.log('Points Missed:');
+console.log(num2 + ", "+ "Score: "+ " " + num5 +"%");
+
